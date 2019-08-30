7773908213a4ec73af00d25c4f0e78e54289d8ad
Saeed Haghtalab <saeed.haghtalab@esss.se>
2019-08-30 13:48:06 +0200

    Update the e3 structure using new e3TemplateGenerator

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# e3-ethmod
+
+e3-ethmod  
+======
+ESS Site-specific EPICS module : ethmod
+
+ethmod repository: https://gitlab.esss.lu.se/beam-diagnostics/bde/epics/modules/ethmod.git
---
+++ cmds/pin_diode.cmd
@@ -0,0 +1,80 @@
+require ethmod,2da1bf4f
+
+# Prefix for all records
+epicsEnvSet("PREFIX",               "TS2-010RFC:RFS-PIND-110")
+epicsEnvSet("AK_IP_PORT_ADDR",      "172.30.4.156:1002")
+
+epicsEnvSet("I2C_TMP100_PORT",      "AK_I2C_TMP100")
+epicsEnvSet("I2C_DS28CM00_PORT",    "AK_I2C_DS28CM00")
+epicsEnvSet("I2C_PCF85063TP_PORT",  "AK_I2C_PCF85063TP")
+epicsEnvSet("I2C_TCA9555_PORT",     "AK_I2C_TCA9555")
+epicsEnvSet("I2C_LTC2991_PORT",     "AK_I2C_LTC2991")
+epicsEnvSet("I2C_M24M02_PORT",      "AK_I2C_M24M02")
+epicsEnvSet("I2C_IP_PORT",          "AK_I2C_COMM")
+
+# Supported IP port types (see AKBase.h)
+#AK_IP_PORT_INVALID       0
+#AK_IP_PORT_RS232         1
+#AK_IP_PORT_RS485         2
+#AK_IP_PORT_LCD           3
+#AK_IP_PORT_I2C           4
+#AK_IP_PORT_SPI           5
+#AK_IP_PORT_TTLIO         6
+#AK_IP_PORT_SDCARD        7
+#AK_IP_PORT_DFCARD        8
+
+###
+# I2C
+###
+# Create the asyn port to talk to the AK-NORD server on command port 1002.
+drvAsynIPPortConfigure($(I2C_IP_PORT),$(AK_IP_PORT_ADDR))
+#asynSetTraceIOMask($(I2C_IP_PORT),0,255)
+#asynSetTraceMask($(I2C_IP_PORT),0,255)
+# Set the terminators
+#asynOctetSetOutputEos($(I2C_IP_PORT), 0, "\003")
+#asynOctetSetInputEos($(I2C_IP_PORT), 0,  "\003")
+
+# AKI2CTMP100Configure(const char *portName, const char *ipPort,
+#        int devCount, const char *devInfos, int priority, int stackSize);
+AKI2CTMP100Configure($(I2C_TMP100_PORT), $(I2C_IP_PORT), 1, "0x49", 1, 0, 0)
+dbLoadRecords("AKI2C_TMP100.db",       "P=$(PREFIX),R=I2C1:Temp1:,PORT=$(I2C_TMP100_PORT),IP_PORT=$(I2C_IP_PORT),ADDR=0,TIMEOUT=1")
+#asynSetTraceIOMask($(I2C_TMP100_PORT),0,255)
+#asynSetTraceMask($(I2C_TMP100_PORT),0,255)
+
+# AKI2CM24M02Configure(const char *portName, const char *ipPort,
+#        int devCount, const char *devInfos, int priority, int stackSize);
+AKI2CM24M02Configure($(I2C_M24M02_PORT), $(I2C_IP_PORT), 1, "0x50", 1, 0, 0)
+dbLoadRecords("AKI2C_M24M02.db",       "P=$(PREFIX),R=I2C1:Eeprom1:,PORT=$(I2C_M24M02_PORT),IP_PORT=$(I2C_IP_PORT),ADDR=0,TIMEOUT=1,NELM=262144")
+#asynSetTraceIOMask($(I2C_M24M02_PORT),0,255)
+#asynSetTraceMask($(I2C_M24M02_PORT),0,255)
+
+# AKI2CTCA9555Configure(const char *portName, const char *ipPort,
+#        int devCount, const char *devInfos, int priority, int stackSize);
+AKI2CTCA9555Configure($(I2C_TCA9555_PORT), $(I2C_IP_PORT), 1, "0x21", 1, 0, 0)
+dbLoadRecords("AKI2C_TCA9555.db",        "P=$(PREFIX),R=I2C1:IOExp1:,PORT=$(I2C_TCA9555_PORT),IP_PORT=$(I2C_IP_PORT),ADDR=0,TIMEOUT=1")
+#asynSetTraceIOMask($(I2C_TCA9555_PORT),0,255)
+#asynSetTraceMask($(I2C_TCA9555_PORT),0,255)
+
+# AKI2CLTC2991Configure(const char *portName, const char *ipPort,
+#        int devCount, const char *devInfos, int priority, int stackSize);
+AKI2CLTC2991Configure($(I2C_LTC2991_PORT), $(I2C_IP_PORT), 1, "0x90", 0, 0, 1, 0, 0)
+dbLoadRecords("AKI2C_LTC2991.db",        "P=$(PREFIX),R=I2C1:VMon1:,PORT=$(I2C_LTC2991_PORT),IP_PORT=$(I2C_IP_PORT),ADDR=0,TIMEOUT=1")
+#asynSetTraceIOMask($(I2C_LTC2991_PORT),0,255)
+#asynSetTraceMask($(I2C_LTC2991_PORT),0,255)
+
+
+#set_requestfile_path("./")
+#set_requestfile_path("$(ETHMOD)/ethmodApp/Db")
+#set_savefile_path("./autosave")
+#set_pass0_restoreFile("auto_settings.sav")
+#set_pass1_restoreFile("auto_settings.sav")
+#save_restoreSet_status_prefix("$(PREFIX)")
+#dbLoadRecords("$(AUTOSAVE)/asApp/Db/save_restoreStatus.db", "P=$(PREFIX)")
+
+#cd "${TOP}/iocBoot/${IOC}"
+iocInit()
+
+# save things every thirty seconds
+#create_monitor_set("${TOP}/iocBoot/${IOC}/auto_settings.req", 30,"P=$(PREFIX)")
+
+dbpf $(PREFIX)I2C1:Temp1:Read.SCAN ".5 second"
