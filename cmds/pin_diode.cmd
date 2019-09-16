3038694d35e9484c9cecde3c61e3e70557234f73
Saeed Haghtalab <saeed.haghtalab@esss.se>
2019-09-16 15:52:02 +0200

    LTC2991 address is 0x48 not 0x90

--- cmds/pin_diode.cmd
+++ cmds/pin_diode.cmd
@@ -62,7 +62,7 @@ dbLoadRecords("AKI2C_TCA9555.db",        "P=$(PREFIX),R=$(R_TCA9555),PORT=$(I2C_
 
 # AKI2CLTC2991Configure(const char *portName, const char *ipPort,
 #        int devCount, const char *devInfos, int priority, int stackSize);
-AKI2CLTC2991Configure($(I2C_LTC2991_PORT), $(I2C_IP_PORT), 1, "0x90", 0, 0, 1, 0, 0)
+AKI2CLTC2991Configure($(I2C_LTC2991_PORT), $(I2C_IP_PORT), 1, "0x48", 0, 0, 1, 0, 0)
 dbLoadRecords("AKI2C_LTC2991.db",        "P=$(PREFIX),R=$(R_LTC2991),PORT=$(I2C_LTC2991_PORT),IP_PORT=$(I2C_IP_PORT),ADDR=0,TIMEOUT=1")
 #asynSetTraceIOMask($(I2C_LTC2991_PORT),0,255)
 #asynSetTraceMask($(I2C_LTC2991_PORT),0,255)
